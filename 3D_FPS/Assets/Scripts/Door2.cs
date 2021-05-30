69f69e6aa886837f0a971addc7e2fbe54b992149
wawrzych <wawrzyczek.bartek@gmail.com>
2021-05-30 10:38:03 +0200

    Submision

---
+++ 3D_FPS/Assets/Scripts/Door2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door1 : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("Open1", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("Open1", false);
+        }
+    }
+}
---
+++ 3D_FPS/Assets/Scripts/Gun.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Rigidbody Bullet;
+    public Transform firePlace;
+    public float power;
+
+    private Rigidbody projectile;
+    private bool fire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            projectile = Instantiate(Bullet, firePlace.position, transform.rotation);
+            fire = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (fire)
+        {
+            projectile.velocity = transform.TransformDirection(Vector3.forward * power);
+            fire = false;
+            Destroy(projectile.gameObject, 3);
+        }
+    }
+}
